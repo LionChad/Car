0df3ffbef6f8bc503f5095a0f8a8337086125aff
88693 <88693@LAPTOP-HMCU1IHA>
2023-09-15 22:21:56 +0800

    封裝-鐵人賽第二日-解說題目

---
+++ Model/CaeModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Car.Model
+{
+    public class CaeModel
+    {
+        private string licensePlate;
+        private string brand;
+        private string color;
+        private bool engineRunning;
+
+        public CaeModel(string licensePlate, string brand, string color)
+        {
+            this.licensePlate = licensePlate;
+            this.brand = brand;
+            this.color = color;
+            this.engineRunning = false;
+        }
+
+        public void StartEngine()
+        {
+            if (!engineRunning)
+            {
+                engineRunning = true;
+                Console.WriteLine("Engine started.");
+            }
+        }
+
+        public void StopEngine()
+        {
+            if (engineRunning)
+            {
+                engineRunning = false;
+                Console.WriteLine("Engine stopped.");
+            }
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            color = newColor;
+            Console.WriteLine($"Car color changed to {newColor}.");
+        }
+    }
+}
