119e4f38831411a897775931781fa051d85c124a
88693 <88693@LAPTOP-HMCU1IHA>
2023-09-16 22:09:04 +0800

    初階面試常見題目回答-繼承-鐵人賽第三日

--- Controllers/GetCarInfomationController.cs
+++ Controllers/GetCarInfomationController.cs
@@ -8,7 +8,33 @@ public class GetCarInfomationController : ControllerBase
     [HttpPost]
     public IActionResult Post([FromBody] GetCarInfomationModel model)
     {
+        ParentClass parentClass = new ParentClass();
+        ChildOverwriteClass childOverwriteClass = new ChildOverwriteClass();
+
+        //"Hi"
+        string parentClassshadowMessage = parentClass.shadowMessage;
+        //"XXX"
+        string parentClassoverridMeessage = parentClass.overridMeessage;
+        //null
+        string parentClassmessage = parentClass.message;
+        //"XXX"
+        string parentClassOverridMeessage = parentClass.OverridMeessage;
+        //null
+        string parentClassMessage = parentClass.Message;
+
+        //shadowMessage遮蓋（shadow）父類別中的 message屬性 : null
+        string childOverwriteClassshadowMessage = childOverwriteClass.shadowMessage;
+        //overridMeessage 調用到父類別 : "XXX"
+        string childOverwriteClassoverridMeessage = childOverwriteClass.overridMeessage;
+        //message 調用到父類別 由於子類別有建構子 : "OOO"
+        string childOverwriteClassmessage = childOverwriteClass.message;
+        //Hello from ChildClass  >> 調用到Overwrite : "Hello from ChildClass"
+        string childOverwriteClassOverridMeessage = childOverwriteClass.OverridMeessage;
+        //Message 調用到父類別 由於子類別有建構子 : "OOO"
+        string childOverwriteClassMessage = childOverwriteClass.Message;
+
         var returnData = model.GetCarInfomation(model);
+
         return Ok(new { message = "Car information received successfully." });
     }
 }
---
+++ Model/ParentClassModel.cs
@@ -0,0 +1,41 @@
+﻿namespace Car.Model
+{
+    class ParentClass
+    {
+        public string shadowMessage;
+        public string overridMeessage;
+        public string message;
+
+        public ParentClass()
+        {
+            shadowMessage = "Hi";
+            overridMeessage = "XXX";
+        }
+
+        public virtual string OverridMeessage
+        {
+            get { return overridMeessage; }
+            set { overridMeessage = value; }
+        }
+        public virtual string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+
+    class ChildOverwriteClass : ParentClass
+    {
+        public new string shadowMessage;
+
+        public ChildOverwriteClass() : base()
+        {
+            message = "OOO";
+        }
+
+        public override string OverridMeessage
+        {
+            get { return "Hello from ChildClass"; } // 在子類別中重寫 Message 屬性的 getter
+        }
+    }
+}
